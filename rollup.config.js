801d1e7a5ed09c77c464a4122442c71cf3852ca3
Alexander Baryshnikov <dev@baryshnikov.net>
2021-03-12 18:52:55 +0800

    generate playground widget

--- rollup.config.js
+++ rollup.config.js
@@ -11,13 +11,14 @@ export default [
     {
         input: 'src/main.js',
         output: {
-            sourcemap: true,
+            sourcemap: false,
             format: 'iife',
             name: 'app',
-            file: 'public/build/bundle.js'
+            file: 'dist/github-card-playground.min.js'
         },
         plugins: [
             svelte({
+                // enable run-time checks when not in production
                 dev: !production,
             }),
             resolve({
@@ -25,8 +26,17 @@ export default [
                 dedupe: ['svelte']
             }),
             commonjs(),
+
+            // In dev mode, call `npm run start` once
+            // the bundle has been generated
             !production && serve(),
+
+            // Watch the `public` directory and refresh the
+            // browser on changes when not in production
             !production && livereload('public'),
+
+            // If we're building for production (npm run build
+            // instead of npm run dev), minify
             production && terser()
         ],
         watch: {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import App from './App.svelte';
 
-const app = new App({
-    target: document.body,
-});
 
-export default app;
+window.GithubCardPlayground = function (options) {
+    return new App(options)
+}
